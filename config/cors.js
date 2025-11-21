c954aa3d51bbe770c5e61d7d6c37668c9af6263b
thunderavi <hittuprasad0909@gmail.com>
2025-11-21 09:50:47 +0530

    update client scoreboard cors

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 // config/cors.js
 module.exports = {
   allowedOrigins: [
-    'https://cricket-scoreboard-react.vercel.app',
+    'https://score-board-seven-tawny.vercel.app',
     'https://live-commentry-45qj.vercel.app/',
     'http://localhost:3000',
     'http://localhost:5173',
